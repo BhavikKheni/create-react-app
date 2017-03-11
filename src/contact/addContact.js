e85ab3e47b4d0707b1f6e7e2b09028f50bd401ff
bhavik kheni <kheni.bhavik@gmail.com>
2017-03-11 15:19:38 +0530

    added contact list component

--- src/contact/addContact.js
+++ src/contact/addContact.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
+import {browserHistory} from 'react-router';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+
 
 const style = {
     margin: 12,
@@ -25,22 +28,41 @@ export class AddContact extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        var newItem = {
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            email: this.state.email,
-            id: Date.now()
-        };
-        this.setState((prevState) => ({
-            items: prevState.items.concat(newItem),
-            firstName: '',
-            lastName: '',
-            email: ''
-        }));
+        const { firstName, lastName, email } = this.state;
+        if (firstName !== '' && lastName !== '' && email !== '') {
+            var newItem = {
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+                email: this.state.email,
+                id: Date.now()
+            };
+            this.setState((prevState) => ({
+                items: prevState.items.concat(newItem),
+                firstName: '',
+                lastName: '',
+                email: ''
+            }));
+        } else {
+            alert('fillp empty field');
+        }
+    }
+    handleClose = () => {
+         browserHistory.push('/');
+    };
+    renderContact() {
+        return this.state.items.map((item, i) => (
+            <TableRow key={i} >
+                <TableRowColumn style={{ textAlign: 'center' }}>{item.firstName}</TableRowColumn>
+                <TableRowColumn style={{ textAlign: 'center' }}>{item.lastName}</TableRowColumn>
+                <TableRowColumn style={{ textAlign: 'center' }}>{item.email}</TableRowColumn>
+            </TableRow>
+        ))
+
+
     }
     render() {
         return (
-            <div className="App">
+            <div className="App" style={{ display: 'flex' }}>
                 <Paper style={style.style} zDepth={5}>
                     <h3>Contact</h3>
                     <form onSubmit={(e) => this.handleSubmit(e)}>
@@ -57,15 +79,32 @@ export class AddContact extends Component {
                             value={this.state.lastName}
                         /><br />
                         <TextField
+                            type="email"
                             hintText="Email"
                             floatingLabelText="Email"
                             onChange={(e) => this.setState({ email: e.target.value })}
                             value={this.state.email}
                         /><br /><br />
                         <RaisedButton label="Save" type="submit" primary={true} style={style} />
-                        <RaisedButton label="Cancel" secondary={true} style={style} />
+                        <RaisedButton label="Cancel" secondary={true} style={style} onTouchTap={this.handleClose} />
                     </form>
                 </Paper>
+
+                <Paper style={style.style} zDepth={5}>
+                    <Table >
+                        <TableHeader displaySelectAll={false}>
+                            <TableRow >
+                                <TableHeaderColumn>FirstName</TableHeaderColumn>
+                                <TableHeaderColumn>LastName</TableHeaderColumn>
+                                <TableHeaderColumn>Email</TableHeaderColumn>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody displayRowCheckbox={false}>
+                            {this.renderContact()}
+                        </TableBody>
+                    </Table>
+                </Paper>
+
             </div>
         );
     }
